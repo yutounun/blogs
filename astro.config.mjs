11eba8aa0de3756c4030963df68b7737fc292038
yutounun <geek.yuto@gmail.com>
2024-04-13 14:14:14 +0700

    fix url for listing on google

--- astro.config.mjs
+++ astro.config.mjs
@@ -15,7 +15,7 @@ const SERVER_PORT = 3000;
 // the url to access your blog during local development
 const LOCALHOST_URL = `http://localhost:${SERVER_PORT}`;
 // the url to access your blog after deploying it somewhere (Eg. Netlify)
-const LIVE_URL = "https://yourwebsiteurl.com";
+const LIVE_URL = "https://dev-whisperer.vercel.app";
 // this is the astro command your npm script runs
 const SCRIPT = process.env.npm_lifecycle_script || "";
 const isBuild = SCRIPT.includes("astro build");
@@ -24,6 +24,8 @@ let BASE_URL = LOCALHOST_URL;
 if (isBuild) {
   BASE_URL = LIVE_URL;
 }
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
