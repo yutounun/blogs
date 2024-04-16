3befa1444a589ef333cf1d60ee21c0446e8ad92b
yutounun <geek.yuto@gmail.com>
2024-04-16 11:27:41 +0700

    move robots.txt into the public dir

--- astro.config.mjs
+++ astro.config.mjs
@@ -28,17 +28,16 @@ if (isBuild) {
 // https://astro.build/config
 
 // https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
   server: {
-    port: SERVER_PORT,
+    port: SERVER_PORT
   },
   site: BASE_URL,
-  integrations: [
-    sitemap(),
-    tailwind({
-      config: {
-        applyBaseStyles: false,
-      },
-    }),
-  ],
-});
+  integrations: [sitemap(), tailwind({
+    config: {
+      applyBaseStyles: false
+    }
+  })]
+}); 
